d71f2184decbd502a24bb43af6da9d7588cab043
gwongz <gwongz@gmail.com>
2014-10-01 14:31:34 -0700

    adding comment

--- client/views/posts/posts_list.js
+++ client/views/posts/posts_list.js
@@ -1,12 +1,28 @@
 /* a manager for doing the heavy lifting in the posts_list template */
 
 // starting with static data 
-
+var postsData = [
+  {
+    title: 'Introducing Telescope',
+    author: 'Sacha Greif',
+    url: 'http://sachagreif.com/introducing-telescope/'
+  },
+  {
+    title: 'Meteor',
+    author: 'Tom Coleman',
+    url: 'http://meteor.com'
+  },
+  {
+    title: 'The Meteor Book',
+    author: 'Tom Coleman',
+    url: 'http://themeteorbook.com'
+  }
+];
 
 // defining the helper for the postsList template 
 Template.postsList.helpers({
-  // return all the objects in the posts collection
-  posts: function(){
-    return Posts.find();
+    // return all objects in the Posts collection
+    posts: function(){
+        return Posts.find();
   }
 });
