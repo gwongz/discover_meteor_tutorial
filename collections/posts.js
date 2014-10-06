0a4818f369ba1e783a5abc3cd8b23185809a1871
gwongz <gwongz@gmail.com>
2014-10-06 15:49:48 -0700

    adding routing hooks for non logged in users

--- collections/posts.js
+++ collections/posts.js
@@ -4,3 +4,14 @@ is a secure copy of subset of real canoncial collection*/
 
 Posts = new Meteor.Collection('posts');
 
+// when we turn off insecure, we have to declare whether client
+// has insert permission
+
+Posts.allow({
+    // this is when clients are allowed to insert into Posts collection
+    insert: function(userId, doc){
+    // only allow posting if you are logged in
+        return !! userId;
+    }
+});
+
