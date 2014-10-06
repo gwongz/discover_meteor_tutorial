7ffb793ec80231cb9fd47f89ad6ee91f5e5736d3
gwongz <gwongz@gmail.com>
2014-10-06 13:00:36 -0700

    adding loading state

--- client/main.js
+++ client/main.js
@@ -1,7 +1,7 @@
 /* How clients specify what data they want to load at that moment */
 
 // returns all posts
-Meteor.subscribe('posts');
+// Meteor.subscribe('posts');
 
 // return posts whose author is bob-smith
 // pick and choose - how to make app scalable and prevent overload of browser memory
